90be2c9680ad052c88d4e26dc4da332562a41b25
Abass Koyang Ahmad <abasskoyang12345@gmail.com>
2025-12-03 18:32:36 +0100

    Update NavBar component for improved responsiveness by adjusting padding, sizes, and layout elements. Enhance search input and button visibility across different screen sizes.

--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -108,24 +108,24 @@ const NavBar = () => {
     }; 
     
   return (
-    <header className='w-full bg-white py-3 lg:py-2 px-4 lg:px-8 flex items-center justify-between'>
+    <header className='w-full bg-white py-3 md:py-2 px-4 md:px-8 flex items-center justify-between'>
         <div className="flex items-center gap-2">
-        <button onClick={() => setIsSidebarOpen(true)} className="block lg:hidden p-2.5 bg-white border border-gray-300 rounded-md relative cursor-pointer">
-            <Menu className='size-[20px] text-gray-800' />
+        <button onClick={() => setIsSidebarOpen(true)} className="block lg:hidden p-2 md:p-2.5 bg-white border border-gray-300 rounded-md relative cursor-pointer">
+            <Menu className='size-[18px] lg:size-[20px] text-gray-800' />
         </button>
-            <div className="border-1 border-gray-300 rounded-lg overflow-hidden w-[200px] lg:w-[300px] group focus-within:border-gray-600 flex items-center justify-between px-4 transition-all duration-200 ease-in-out">
+            <div className="border-1 border-gray-300 rounded-lg overflow-hidden w-[180px] md:w-[300px] group focus-within:border-gray-600 flex items-center justify-between px-4 transition-all duration-200 ease-in-out">
                 <Search className='group-focus-within:text-gray-800 text-gray-500 size-[16px] transition-all duration-200 ease-in-out' />
                 <input type="text"                 value={searchQuery}
-                    onChange={(e) => setSearchQuery(e.target.value)} className="w-[90%] h-[40px] px-2 text-sm text-gray-800 font-nunito-sans placeholder:text-gray-500 border-0 stroke-0 outline-0 transition-all duration-200 ease-in-out" placeholder='Search by title...'/>
+                    onChange={(e) => setSearchQuery(e.target.value)} className="w-[90%] h-[35px] md:h-[40px] px-2 text-sm text-gray-800 font-nunito-sans placeholder:text-gray-500 border-0 stroke-0 outline-0 transition-all duration-200 ease-in-out" placeholder='Search by title...'/>
             </div>
         </div>
 
         <div className="">
             {user ? (
                     <div className='flex items-center gap-2'>
-                        <button onClick={() => setIsFormOpen(true)} className='px-2 lg:px-4 py-2 rounded-md bg-dark-blue text-white font-medium font-raleway text-sm flex items-center gap-1.5 cursor-pointer'>
+                        <button onClick={() => setIsFormOpen(true)} className='px-2 md:px-4 py-2 rounded-md bg-dark-blue text-white font-medium font-raleway text-sm flex items-center gap-1.5 cursor-pointer'>
                             <Plus className='size-[18px] text-white' />
-                            <span className='hidden lg:block'>Add Bookmark</span>
+                            <span className='hidden md:block'>Add Bookmark</span>
                         </button>
                         <div ref={containerRef} onClick={() => setIsSignOutOpen(true)} className="relative">
                             <div className='size-[40px] rounded-full overflow-hidden'>
@@ -149,7 +149,7 @@ const NavBar = () => {
                         ) : (
                             <>
                             <UserRoundPlus className='size-[18px] text-white' />
-                            <span className='hidden lg:block'>Sign In</span></>
+                            <span className='hidden md:block'>Sign In</span></>
                     )}
                     </button>
             )}
